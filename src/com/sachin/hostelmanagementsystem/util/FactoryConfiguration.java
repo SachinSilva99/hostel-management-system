23c67daad7f89b5aa6607f38069bbbf3f390b55d
SachinSilva99 <sachinvalo99@gmail.com>
2023-03-30 09:34:28 -0700

    added hibernate.properties file

--- src/com/sachin/hostelmanagementsystem/util/FactoryConfiguration.java
+++ src/com/sachin/hostelmanagementsystem/util/FactoryConfiguration.java
@@ -7,13 +7,27 @@ import com.sachin.hostelmanagementsystem.entity.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.jpamodelgen.xml.jaxb.Persistence;
+
+import java.io.IOException;
 
 public class FactoryConfiguration {
-    private static final FactoryConfiguration factoryConfiguration = new FactoryConfiguration();
+    private static final FactoryConfiguration factoryConfiguration;
+
+    static {
+        try {
+            factoryConfiguration = new FactoryConfiguration();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     private final SessionFactory sessionFactory;
 
-    private FactoryConfiguration() {
-        Configuration configure = new Configuration().configure("hibernate.cfg.xml")
+    private FactoryConfiguration() throws IOException {
+
+        Configuration configure = new Configuration();
+        configure
                 .addAnnotatedClass(Reservation.class)
                 .addAnnotatedClass(Room.class)
                 .addAnnotatedClass(Student.class);
